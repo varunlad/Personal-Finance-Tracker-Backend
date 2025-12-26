e59e4c0a2b5ef5ee4bbe4f1b4dd06a334ae23419
varunlad <varunlad5@gmail.com>
2025-12-27 01:11:36 +0530

    Created Backend for Personal Finace Tracker project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+  
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
